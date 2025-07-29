33c7acf871cb2f65fbc3cec18d26fc86a5abd791
Serge042 <sakovtun412@gmail.com>
2025-07-29 12:51:12 +0300

    Create IInterestCalculator.cs

---
+++ IInterestCalculator.cs
@@ -0,0 +1,20 @@
+public interface IInterestCalculator
+{
+    double CalculateInterest(Account account);
+}
+
+public class OrdinaryAccountCalculator : IInterestCalculator
+{
+    public double CalculateInterest(Account account)
+    {
+        double interest = account.Balance * 0.4;
+        
+        if (account.Balance < 1000)
+            interest -= account.Balance * 0.2;
+        
+        if (account.Balance >= 1000)
+            interest -= account.Balance * 0.4;
+        
+        return interest;
+    }
+}
